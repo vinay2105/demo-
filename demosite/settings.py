77a2971778ba91935f5ff415086222403e551d85
unknown <kvinay21505@gmail.com>
2026-01-22 14:26:03 +0530

    allowed host

--- demosite/settings.py
+++ demosite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-please-change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
