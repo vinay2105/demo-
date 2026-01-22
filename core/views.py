db98ecbace23e9aedc8233aa8ba71359fae49c44
unknown <kvinay21505@gmail.com>
2026-01-22 14:16:43 +0530

    first

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def services(request):
+    return render(request, 'core/services.html')
